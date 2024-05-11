757c41e52039296bbd56bd5430e07d284a9c80aa
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-05-11 16:26:16 -0700

    Home page: add vector grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,117 @@
 import Link from 'next/link'
+import Image from 'next/image'
+
+import { Grid } from '@/components/Grid'
+import { Spacer } from '@/components/Spacer'
+
+import styles from './Home.module.css'
 
 export default function Home() {
   return (
     <>
       <header>
         <h1>compsigh</h1>
+        <p className={styles.description}>
+          compsigh is a social computer science club for meeting cool people and building cool things.
+          <br />
+          We host high-quality, low-stakes events, and engage in a vibrant, family-like community.
+        </p>
       </header>
+      <Grid
+        columns={3}
+        style={{ placeItems: 'center' }}
+      >
+        <Image
+          src="/assets/E3.svg"
+          alt=""
+          width={20}
+          height={20}
+        />
+        <Image
+          src="/assets/E1.svg"
+          alt=""
+          width={20}
+          height={20}
+        />
+        <Image
+          src="/assets/H4.svg"
+          alt=""
+          width={20}
+          height={20}
+        />
+      </Grid>
+      <Spacer size={10} />
+      <Grid
+        columns={3}
+        style={{ placeItems: 'center' }}
+      >
+        <Image
+          src="/assets/B3.svg"
+          alt=""
+          width={20}
+          height={20}
+        />
+        <Image
+          src="/assets/E4.svg"
+          alt=""
+          width={20}
+          height={20}
+        />
+        <Image
+          src="/assets/C4.svg"
+          alt=""
+          width={20}
+          height={20}
+        />
+      </Grid>
+      <Spacer size={10} />
+      <Grid
+        columns={3}
+        style={{ placeItems: 'center' }}
+      >
+        <Image
+          src="/assets/C1.svg"
+          alt=""
+          width={20}
+          height={20}
+        />
+        <Image
+          src="/assets/A2.svg"
+          alt=""
+          width={20}
+          height={20}
+        />
+        <Image
+          src="/assets/F4.svg"
+          alt=""
+          width={20}
+          height={20}
+        />
+      </Grid>
+      <Spacer size={10} />
+      <Grid
+        columns={3}
+        style={{ placeItems: 'center' }}
+      >
+        <Image
+          src="/assets/H1.svg"
+          alt=""
+          width={20}
+          height={20}
+        />
+        <Image
+          src="/assets/D1.svg"
+          alt=""
+          width={20}
+          height={20}
+        />
+        <Image
+          src="/assets/H3.svg"
+          alt=""
+          width={20}
+          height={20}
+        />
+      </Grid>
       <nav>
         <ul>
           <li>
@@ -16,7 +122,6 @@ export default function Home() {
           </li>
         </ul>
       </nav>
-      <p>compsigh is a social computer science club for meeting cool people and building cool things. We host high-quality, low-stakes events, and engage in a vibrant, family-like community.</p>
     </>
   )
 }
---
+++ components/Spacer/Spacer.tsx
@@ -0,0 +1,6 @@
+export function Spacer(
+  { size }:
+  { size: number | string }
+) {
+  return <div style={{ height: size }} />
+}
