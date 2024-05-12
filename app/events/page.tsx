e9f508e1e23a67d868850e46884f2d5a58703b0d
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-05-12 12:21:41 -0700

    Add h2 styles

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -2,7 +2,7 @@ export default function Events() {
   return (
     <>
       <header>
-        <h1>Events</h1>
+        <h2>Events</h2>
       </header>
       <p>compsigh events:</p>
       <p>(to be implemented via Notion API)</p>
