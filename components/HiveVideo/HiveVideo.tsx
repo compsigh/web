5c72d22c6f834a4cbc03d0cd846f6bfe1c949460
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-07-01 17:37:25 -0700

    Compose `HiveVideo`

---
+++ components/HiveVideo/HiveVideo.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { useEffect, useRef } from 'react'
+
+import styles from './HiveVideo.module.css'
+
+export function HiveVideo() {
+  const videoRef = useRef<HTMLVideoElement>(null)
+
+  useEffect(() => {
+    function handleMouseMove(event: MouseEvent) {
+      const { clientX, clientY } = event
+      const { innerWidth, innerHeight } = window
+      const offsetX = ((clientX / innerWidth) - 0.5) * -2
+      const offsetY = ((clientY / innerHeight) - 0.5) * -2
+      console.log(offsetX, offsetY)
+
+      if (videoRef.current)
+        videoRef.current.style.transform = `translate(${-50 + offsetX * 5}%, ${-50 + offsetY * 5}%)`
+    }
+
+    window.addEventListener('mousemove', handleMouseMove)
+    return () => window.removeEventListener('mousemove', handleMouseMove)
+  }, [])
+
+  return (
+    <>
+      <div className={styles.container}>
+        <video
+          ref={videoRef}
+          className={styles.video}
+          src="/assets/hive-loop-compressed.mp4"
+          autoPlay
+          loop
+          muted
+        />
+      </div>
+    </>
+  )
+}
---
+++ components/HiveVideo/index.ts
@@ -0,0 +1,1 @@
+export { HiveVideo } from './HiveVideo'
