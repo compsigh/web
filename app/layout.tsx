ae729c6442348790a0c8392e36f01dac6f2979e8
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-07-01 17:13:19 -0700

    Remove `Footer`

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ import localFont from 'next/font/local'
 import { Grid } from '@/components/Grid'
 import { Spacer } from '@/components/Spacer'
 import { Navbar } from '@/components/Navbar'
-import { Footer } from '@/components/Footer'
 const Tape = dynamic(() => import('@/components/Tape').then((mod) => mod.Tape), { ssr: false })
 
 const Delko = localFont({
@@ -130,7 +129,6 @@ export default function RootLayout({
           {children}
         </main>
         <Tape />
-        <Footer />
       </body>
     </html>
   )
