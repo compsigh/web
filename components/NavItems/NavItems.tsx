595b1c4e50d2b835ed4c2fda8359313801f45c50
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-07-07 22:21:09 -0700

    Add mobile styles to landing page

--- components/NavItems/NavItems.tsx
+++ components/NavItems/NavItems.tsx
@@ -5,9 +5,6 @@ export function NavItems() {
     <>
       <ul>
         <li>
-          <Link href={`/events`}>Events</Link>
-        </li>
-        <li>
           <Link href={`https://discord.compsigh.club`}>Discord</Link>
         </li>
         <li>
