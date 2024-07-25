c4f9afb9de1367eeff86f7c0f02b2c56490cce32
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-07-25 11:43:02 -0700

    Add emotes

--- components/Decorations/Emote/Emote.tsx
+++ components/Decorations/Emote/Emote.tsx
@@ -3,49 +3,69 @@ import Image from 'next/image'
 import './Emote.css'
 
 export type Emote
-  = 'compsighLogo'
-  | 'hive'
-  | 'bensonstare'
+  = 'bensonstare'
   | 'buildingBlocks'
+  | 'c'
   | 'calendar'
-  | 'cascade'
+  | 'compsighLogo'
   | 'cue'
+  | 'deploy23'
   | 'heart'
+  | 'hive'
+  | 'java'
   | 'mehmote'
   | 'moon'
   | 'nextjs'
+  | 'nixos'
+  | 'nodejs'
   | 'notepad'
   | 'openBook'
   | 'philmote'
   | 'picnic'
   | 'playingCards'
+  | 'python'
+  | 'react'
   | 'repost'
   | 'rust'
   | 'sunflowers'
   | 'taoFedora'
+  | 'tunnel'
+  | 'typescript'
+  | 'usf'
+  | 'ventrue'
   | 'vercel'
 
 export const emoteSrcs: Record<Emote, string> = {
-  compsighLogo: '/assets/compsigh-logo.svg',
-  hive: '/icons/hive.svg',
   bensonstare: '/emotes/bensonstare.png',
   buildingBlocks: '/emotes/building-blocks.svg',
+  c: '/emotes/c.svg',
   calendar: '/emotes/calendar.svg',
-  cascade: '/emotes/cascade.png',
+  compsighLogo: '/assets/compsigh-logo.svg',
   cue: '/emotes/cue.svg',
+  deploy23: '/emotes/deploy23.svg',
   heart: '/emotes/heart.svg',
+  hive: '/icons/hive.svg',
+  java: '/emotes/java.svg',
   mehmote: '/emotes/mehmote.png',
   moon: '/emotes/moon.svg',
   nextjs: '/emotes/nextjs.svg',
+  nixos: '/emotes/nixos.svg',
+  nodejs: '/emotes/nodejs.svg',
   notepad: '/emotes/notepad.svg',
   openBook: '/emotes/open-book.svg',
   philmote: '/emotes/philmote.png',
   picnic: '/emotes/picnic.svg',
   playingCards: '/emotes/playing-cards.svg',
-  repost: '/emotes/repost.png',
+  python: '/emotes/python.svg',
+  react: '/emotes/react.svg',
+  repost: '/emotes/repost.svg',
   rust: '/emotes/rust.svg',
   sunflowers: '/emotes/sunflowers.png',
   taoFedora: '/emotes/tao-fedora.png',
+  tunnel: '/emotes/tunnel.svg',
+  typescript: '/emotes/typescript.svg',
+  usf: '/emotes/usf.png',
+  ventrue: '/emotes/ventrue.svg',
   vercel: '/emotes/vercel.svg'
 }
 
