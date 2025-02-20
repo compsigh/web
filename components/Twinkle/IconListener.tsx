29a0aae4962050935fe7a79fc602de68fa61b02c
Matthew Stanciu <mattbstanciu@gmail.com>
2025-02-20 17:37:21 -0500

    twinkle translate

--- components/Twinkle/IconListener.tsx
+++ components/Twinkle/IconListener.tsx
@@ -10,22 +10,26 @@ export function IconListener({
   const iconRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
+    const prefersReducedMotion = window.matchMedia(`(prefers-reduced-motion: reduce)`).matches;
+    
     function handleMouseMove(event: MouseEvent) {
-      const icon = iconRef.current
-      if (!icon) return
-
-      const { clientX, clientY } = event
-      const { innerWidth, innerHeight } = window
-      const centerX = innerWidth / 2
-      const centerY = innerHeight / 2
-
-      const distanceX = clientX - centerX
-      const distanceY = clientY - centerY
-
-      const offsetX = (distanceX / innerWidth) * scale * 500
-      const offsetY = (distanceY / innerHeight) * scale * 500
-
-      icon.style.transform = `translate(${offsetX}%, ${offsetY}%)`
+      if (!prefersReducedMotion) {
+        const icon = iconRef.current
+        if (!icon) return
+  
+        const { clientX, clientY } = event
+        const { innerWidth, innerHeight } = window
+        const centerX = innerWidth / 2
+        const centerY = innerHeight / 2
+  
+        const distanceX = clientX - centerX
+        const distanceY = clientY - centerY
+  
+        const offsetX = (distanceX / innerWidth) * scale * 500
+        const offsetY = (distanceY / innerHeight) * scale * 500
+  
+        icon.style.transform = `translate(${offsetX}%, ${offsetY}%)` 
+      } 
     }
 
     window.addEventListener('mousemove', handleMouseMove)
