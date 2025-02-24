4703798bb7fd46a97d66f3f3e2bd0a0fcfc8d4fd
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2025-02-24 14:25:48 -0800

    Use CSS Modules for `Emote`

--- components/Decorations/Emote/Emote.tsx
+++ components/Decorations/Emote/Emote.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 
-import './Emote.css'
+import styles from './Emote.module.css'
 
 type Emote
   = 'bensonstare'
@@ -49,56 +49,56 @@ type Emote
   | 'warp'
 
 const emoteSrcs: Record<Emote, string> = {
-  bensonstare: '/emotes/bensonstare.png',
-  c: '/emotes/c.svg',
-  calendar: '/emotes/calendar.svg',
-  clone: '/emotes/clone.svg',
+  bensonstare:  '/emotes/bensonstare.png',
+  c:            '/emotes/c.svg',
+  calendar:     '/emotes/calendar.svg',
+  clone:        '/emotes/clone.svg',
   compsighLogo: '/assets/compsigh-logo.svg',
-  cue: '/emotes/cue.svg',
-  deploy23: '/emotes/deploy23.svg',
-  figma: '/emotes/figma.svg',
-  github: '/emotes/github.svg',
-  go: '/emotes/go.png',
-  haskell: '/emotes/haskell.svg',
-  heart: '/emotes/heart.svg',
-  hive: '/emotes/hive.svg',
+  cue:          '/emotes/cue.svg',
+  deploy23:     '/emotes/deploy23.svg',
+  figma:        '/emotes/figma.svg',
+  github:       '/emotes/github.svg',
+  go:           '/emotes/go.png',
+  haskell:      '/emotes/haskell.svg',
+  heart:        '/emotes/heart.svg',
+  hive:         '/emotes/hive.svg',
   intellijidea: '/emotes/intellijidea.svg',
-  java: '/emotes/java.svg',
-  leetcode: '/emotes/leetcode.png',
-  linux: '/emotes/linux.png',
-  markdown: '/emotes/markdown.png',
-  mehmote: '/emotes/mehmote.png',
-  neovim: '/emotes/neovim.svg',
-  nextjs: '/emotes/nextjs.svg',
-  nixos: '/emotes/nixos.svg',
-  nodejs: '/emotes/nodejs.svg',
-  notepad: '/emotes/notepad.svg',
-  obsidian: '/emotes/obsidian.png',
-  openBook: '/emotes/open-book.svg',
-  philmote: '/emotes/philmote.png',
-  picnic: '/emotes/picnic.svg',
+  java:         '/emotes/java.svg',
+  leetcode:     '/emotes/leetcode.png',
+  linux:        '/emotes/linux.png',
+  markdown:     '/emotes/markdown.png',
+  mehmote:      '/emotes/mehmote.png',
+  neovim:       '/emotes/neovim.svg',
+  nextjs:       '/emotes/nextjs.svg',
+  nixos:        '/emotes/nixos.svg',
+  nodejs:       '/emotes/nodejs.svg',
+  notepad:      '/emotes/notepad.svg',
+  obsidian:     '/emotes/obsidian.png',
+  openBook:     '/emotes/open-book.svg',
+  philmote:     '/emotes/philmote.png',
+  picnic:       '/emotes/picnic.svg',
   playingCards: '/emotes/playing-cards.svg',
-  python: '/emotes/python.png',
-  react: '/emotes/react.svg',
-  repost: '/emotes/repost.svg',
-  rust: '/emotes/rust.svg',
-  sunflowers: '/emotes/sunflowers.png',
-  taoFedora: '/emotes/tao-fedora.png',
-  threejs: '/emotes/threejs.svg',
-  tunnel: '/emotes/tunnel.svg',
-  typescript: '/emotes/typescript.svg',
-  usf: '/emotes/usf.png',
-  ventrue: '/emotes/ventrue.svg',
-  vercel: '/emotes/vercel.svg',
-  vim: '/emotes/vim.svg',
-  vscode: '/emotes/vscode.png',
-  warp: '/emotes/warp.png',
+  python:       '/emotes/python.png',
+  react:        '/emotes/react.svg',
+  repost:       '/emotes/repost.svg',
+  rust:         '/emotes/rust.svg',
+  sunflowers:   '/emotes/sunflowers.png',
+  taoFedora:    '/emotes/tao-fedora.png',
+  threejs:      '/emotes/threejs.svg',
+  tunnel:       '/emotes/tunnel.svg',
+  typescript:   '/emotes/typescript.svg',
+  usf:          '/emotes/usf.png',
+  ventrue:      '/emotes/ventrue.svg',
+  vercel:       '/emotes/vercel.svg',
+  vim:          '/emotes/vim.svg',
+  vscode:       '/emotes/vscode.png',
+  warp:         '/emotes/warp.png',
 }
 
 export function Emote({ emote }: { emote: Emote }) {
   return (
     <Image
-      className='emoteImage'
+      className={styles.emote}
       src={emoteSrcs[emote]}
       alt={emote}
       width={24}
