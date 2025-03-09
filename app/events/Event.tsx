056b57313033a4ec8dbf9cb6b32bc3b91f5340a2
Thomas Yeung <yeung.thomas.h@gmail.com>
2025-03-08 22:54:04 -0800

    fix event id always being in utc

--- app/events/Event.tsx
+++ app/events/Event.tsx
@@ -14,7 +14,7 @@ export function Event({ event }: { event: EventFrontmatter }) {
   const year = new Date(start * 1000).getFullYear()
   const startDate = new Date(start * 1000).toLocaleString('en-US', { timeZone: 'America/Los_Angeles', month: 'short', day: '2-digit', year: year === currentYear ? undefined : 'numeric' }).replace(',', '')
   const startTime = new Date(start * 1000).toLocaleString('en-US', { timeZone: 'America/Los_Angeles', hour: '2-digit', minute: '2-digit' })
-  const idStartDate = new Date(start * 1000).toISOString().split('T')[0]
+  const idStartDate = new Date(start * 1000).toLocaleDateString('sv-SE', { timeZone: 'America/Los_Angeles'})
 
   const currentUnixTimestamp = Math.floor(Date.now() / 1000)
   function isEventHappeningNow(start: number, end: number) {
