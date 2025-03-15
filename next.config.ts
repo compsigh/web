392ba550f7f87ae54ba7936550c7084bc18ed948
Edward Shturman <60242511+edwardshturman@users.noreply.github.com>
2025-03-15 03:21:52 -0700

    Enable PPR on `/community` (#29)
    * Use `next` `15.3.0-canary.9`
    
    * Nit quote consistency
    
    * Enable PPR on `/community`

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import { type NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  experimental: {
+    ppr: 'incremental'
+  },
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
   reactStrictMode: true,
   transpilePackages: ['next-mdx-remote']
