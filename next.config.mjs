e77d410c186ea6dfd3851411a3f2981d1b26b13f
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2025-03-15 02:00:54 -0700

    Fix `next-mdx-remote` dev server crash

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,8 @@
  **/
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  reactStrictMode: true
+  reactStrictMode: true,
+  transpilePackages: ['next-mdx-remote']
 }
 
 export default nextConfig
