be969fd17496cd08740ac869d8684bf672f80e83
Jet Pham <jetthomaspham@gmail.com>
2025-08-18 23:32:58 -0700

    feat: remove unused spotify directory

--- app/spotify/page.tsx
+++
@@ -1,9 +0,0 @@
-import { get } from '@vercel/edge-config'
-import { redirect } from 'next/navigation'
-
-export default async function Spotify() {
-  const spotify = await get('spotify')
-  if (!spotify || spotify.toString() === '')
-    redirect('/')
-  redirect(spotify.toString())
-}
