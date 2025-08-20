3db075bb18fb8d9628ea525751321ede378e936c
Jet Pham <jetthomaspham@gmail.com>
2025-08-20 15:12:44 -0700

    feat: update deps to matching for react 19 (#67)
    * feat: update deps to matching for react 19
    
    * feat: remvoe empty file
    
    * feat: add packages and lock

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,16 @@
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import { FlatCompat } from '@eslint/eslintrc'
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginNext from '@next/eslint-plugin-next';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-})
-
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript')
-]
-
-export default eslintConfig
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      '@next/next': pluginNext,
+    },
+    rules: {
+      ...pluginNext.configs.recommended.rules,
+    },
+  },
+];
