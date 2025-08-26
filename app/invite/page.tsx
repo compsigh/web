26ebf1547d5aeb67e57f5a90e9c54dcf5d3f0c86
Jet Pham <jetthomaspham@gmail.com>
2025-08-26 14:20:26 -0700

    feat: add invite directory and edge config (#75)

---
+++ app/invite/page.tsx
@@ -0,0 +1,9 @@
+import { get } from '@vercel/edge-config'
+import { redirect } from 'next/navigation'
+
+export default async function Invite() {
+  const invite = await get('invite')
+  if (!invite || invite.toString() === '')
+    redirect('/')
+  redirect(invite.toString())
+}
