36634ab9d30ea4ffb2cbda409c16a0ebcf5d54ba
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2025-11-28 01:01:20 -0800

    Add feature flag for toggling marquee with Toolbar

---
+++ flags.ts
@@ -0,0 +1,13 @@
+import { flag } from "flags/next"
+import { get } from "@vercel/edge-config"
+
+export const showMarquee = flag({
+  key: "show-marquee",
+  description: "Display the /workingon marquee on the Community page",
+  defaultValue: false,
+  decide: async function () {
+    return (await get(this.key)) as boolean
+  }
+})
+
+export const flags = [showMarquee] as const
