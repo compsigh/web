a167d093049fcf530249ea523b356cd0083d7970
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2025-12-12 01:46:17 -0800

    Remove ESLint plugin bloatware

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,16 @@
-import next from "eslint-config-next"
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals"
-import nextTypescript from "eslint-config-next/typescript"
-import js from "@eslint/js"
-import tseslint from "typescript-eslint"
-import pluginNext from "@next/eslint-plugin-next"
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-export default [
-  ...next,
-  ...nextCoreWebVitals,
-  ...nextTypescript,
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    plugins: {
-      "@next/next": pluginNext
-    },
-    rules: {
-      ...pluginNext.configs.recommended.rules
-    }
-  },
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname
+})
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
       "node_modules/**",
@@ -29,3 +21,5 @@ export default [
     ]
   }
 ]
+
+export default eslintConfig
