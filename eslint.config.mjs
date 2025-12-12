aaca10ce538660b6369c16ce3218199ed0f5547e
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2025-12-12 01:36:31 -0800

    Migrate to ESLint CLI
    Per Next.js 15.5: https://nextjs.org/blog/next-15-5
    
    `pnpx @next/codemod@latest next-lint-to-eslint-cli .`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,31 @@
-import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import pluginNext from '@next/eslint-plugin-next';
+import next from "eslint-config-next"
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals"
+import nextTypescript from "eslint-config-next/typescript"
+import js from "@eslint/js"
+import tseslint from "typescript-eslint"
+import pluginNext from "@next/eslint-plugin-next"
 
 export default [
+  ...next,
+  ...nextCoreWebVitals,
+  ...nextTypescript,
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
     plugins: {
-      '@next/next': pluginNext,
+      "@next/next": pluginNext
     },
     rules: {
-      ...pluginNext.configs.recommended.rules,
-    },
+      ...pluginNext.configs.recommended.rules
+    }
   },
-];
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts"
+    ]
+  }
+]
