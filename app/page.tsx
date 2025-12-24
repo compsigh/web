cd2c3b104aa44ebbf5b6ba928f8ebbc763c43f86
toashid-tech <toashid@gmail.com>
2025-12-23 20:52:39 -0800

    getting time and comparing

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
-import { NavItems } from '@/components/NavItems'
-import { HomeLoop } from '@/components/HomeLoop'
+import { NavItems } from "@/components/NavItems";
+import { HomeLoop } from "@/components/HomeLoop";
 import { HomepageBanner } from "@/components/HomepageBanner";
+import { getEvents } from "./events/page";
 
-import styles from './Home.module.css'
+import styles from "./Home.module.css";
 
-export default function Home() {
+export default async function Home() {
+  const events = await getEvents();
+
   return (
     <>
       <div id={styles.content}>
-        <HomepageBanner />
+        <HomepageBanner events={events} />
         <h1 id={styles.title}>compsigh</h1>
         <p id={styles.description}>
-          compsigh is the social computer science club at USFCA for meeting cool people &amp;&amp; building cool things. We host high-quality, low-stakes events, and engage in a vibrant community.
+          compsigh is the social computer science club at USFCA for meeting cool
+          people &amp;&amp; building cool things. We host high-quality,
+          low-stakes events, and engage in a vibrant community.
         </p>
         <nav id={styles.navbar}>
           <NavItems />
@@ -21,5 +26,5 @@ export default function Home() {
         <HomeLoop />
       </div>
     </>
-  )
+  );
 }
--- components/HomepageBanner/HomepageBanner.tsx
+++ components/HomepageBanner/HomepageBanner.tsx
@@ -1,8 +1,40 @@
-import { getEvents } from "@/app/events/page";
+"use client";
 import { EventFrontmatter } from "@/app/events/page";
+import Link from "next/link";
 
-export async function HomepageBanner() {
-  const events: EventFrontmatter[] = await getEvents();
-  console.log(events);
-  return <h1>HEllO</h1>;
+export function HomepageBanner({ events }: { events: EventFrontmatter[] }) {
+  const currentTime = Math.floor(new Date().getTime() / 1000);
+  if (events.length === 0) return <></>;
+
+  const updatedEvents: EventFrontmatter[] = [
+    ...events,
+    {
+      title: "DAVE",
+      description: "register!",
+      event_details: {
+        start: 1766551860,
+        end: 9999999999999,
+        location: "The Hive",
+        cover_image: "/events/2025-11-07/deploy25.png",
+        pictures: [],
+        link: "https://touch-grass.com",
+      },
+      slug: "events/2025-11-07/deploy25",
+    },
+  ];
+
+  const upcomingEvents: EventFrontmatter[] = [];
+  for (const event of updatedEvents)
+    if (currentTime < event.event_details.start) upcomingEvents.push(event);
+
+  if (upcomingEvents.length === 0) return <></>;
+  const nearestUpcomingEvent = upcomingEvents[0];
+  const title = nearestUpcomingEvent.title;
+  const eventLink = nearestUpcomingEvent.event_details.link!;
+
+  return (
+    <Link href={eventLink}>
+      <h1>&quot;{title}&quot; is coming up.</h1>
+    </Link>
+  );
 }
