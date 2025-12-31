0b9c6791333e8f5555bc70a65d461f369545c080
Edward Shturman <60242511+edwardshturman@users.noreply.github.com>
2025-12-30 19:58:33 -0800

    Add Prettier config (#117)
    * Add `prettier` dev dep
    
    * Add Prettier config
    
    * Run Prettier on a few pages
    
    * Add `pretty` script

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import { NavItems } from '@/components/NavItems'
-import { HomeLoop } from '@/components/HomeLoop'
+import { NavItems } from "@/components/NavItems"
+import { HomeLoop } from "@/components/HomeLoop"
 
-import styles from './Home.module.css'
+import styles from "./Home.module.css"
 
 export default function Home() {
   return (
@@ -9,7 +9,9 @@ export default function Home() {
       <div id={styles.content}>
         <h1 id={styles.title}>compsigh</h1>
         <p id={styles.description}>
-          compsigh is the social computer science club at USFCA for meeting cool people &amp;&amp; building cool things. We host high-quality, low-stakes events, and engage in a vibrant community.
+          compsigh is the social computer science club at USFCA for meeting cool
+          people &amp;&amp; building cool things. We host high-quality,
+          low-stakes events, and engage in a vibrant community.
         </p>
         <nav id={styles.navbar}>
           <NavItems />
