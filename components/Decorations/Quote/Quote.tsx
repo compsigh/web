01cbf81640106452ddb0d39a1b70b9e2c56ec97d
Edward Shturman <60242511+edwardshturman@users.noreply.github.com>
2026-02-13 03:04:42 -0800

    Wrap `Math.random()` invocations in state (#128)

--- components/Decorations/Quote/Quote.tsx
+++ components/Decorations/Quote/Quote.tsx
@@ -1,4 +1,8 @@
-import styles from './Quote.module.css'
+"use client"
+
+import { useState } from "react"
+
+import styles from "./Quote.module.css"
 
 type Quote = {
   quote: string
@@ -7,75 +11,77 @@ type Quote = {
 
 export const quotes: Quote[] = [
   {
-    quote: 'shit does NOT work on my machine',
-    author: 'Dave'
+    quote: "shit does NOT work on my machine",
+    author: "Dave"
   },
   {
-    quote: 'be ready for some vibe kernel coding next week',
-    author: 'Professor Benson'
+    quote: "be ready for some vibe kernel coding next week",
+    author: "Professor Benson"
   },
   {
-    quote: 'it will not be secure',
-    author: 'Pete'
+    quote: "it will not be secure",
+    author: "Pete"
   },
   {
-    quote: 'I had a dream that I was sleeping in a Docker container',
-    author: 'Dave'
+    quote: "I had a dream that I was sleeping in a Docker container",
+    author: "Dave"
   },
   {
-    quote: 'meow',
-    author: 'Gursh'
+    quote: "meow",
+    author: "Gursh"
   },
   {
-    quote: 'fuck it im deleting everything',
-    author: 'Nish'
+    quote: "fuck it im deleting everything",
+    author: "Nish"
   },
   {
-    quote: 'I could not be more powered by AI right now',
-    author: 'Jet'
+    quote: "I could not be more powered by AI right now",
+    author: "Jet"
   },
   {
     quote: "it's Python; type errors aren't real",
-    author: 'Jet'
+    author: "Jet"
   },
   {
-    quote: 'using machine learning to rate how well you are mewing',
-    author: 'Faadil'
+    quote: "using machine learning to rate how well you are mewing",
+    author: "Faadil"
   },
   {
-    quote: 'Just tried to auto-pair brackets on paper',
-    author: 'Tao'
+    quote: "Just tried to auto-pair brackets on paper",
+    author: "Tao"
   },
   {
     quote: "idk who atoi() ppl are but as an INTJ I don't like the sound of it",
-    author: 'Faadil'
+    author: "Faadil"
   },
   {
     quote: "idk I think I'm fucked",
-    author: 'Eric'
+    author: "Eric"
   },
   {
-    quote: '"iteration velocity" is us committing tax fraud by the end of next week',
-    author: 'Jet'
+    quote:
+      '"iteration velocity" is us committing tax fraud by the end of next week',
+    author: "Jet"
   },
   {
-    quote: 'just core dumped in my pants a bit',
-    author: 'Jet'
+    quote: "just core dumped in my pants a bit",
+    author: "Jet"
   },
   {
-    quote: 'Java code makes the reader have cache misses in their brain',
-    author: 'Tao'
+    quote: "Java code makes the reader have cache misses in their brain",
+    author: "Tao"
   }
 ]
 
 export function Quote({ quote }: { quote: Quote }) {
+  // Random rotation between -10 and 10 degrees
+  const [rotation] = useState(() => Math.random() * 20 - 10)
   return (
     <>
       <div
         id={styles["quote-container"]}
         style={{
-          // Random rotation between -10 and 10 degrees
-          transform: `rotate(${Math.random() * 20 - 10}deg)`
+          transform: `rotate(${rotation}deg)`
         }}
       >
         <blockquote>
@@ -88,10 +94,10 @@ export function Quote({ quote }: { quote: Quote }) {
 }
 
 export function RandomQuote() {
-  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+  const [index] = useState(() => Math.floor(Math.random() * quotes.length))
   return (
     <>
-      <Quote quote={randomQuote} />
+      <Quote quote={quotes[index]} />
     </>
   )
 }
