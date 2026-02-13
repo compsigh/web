d7c91f258f33cf9fe4e667339c84375972bec7f7
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2026-02-13 01:21:34 -0800

    Centralize Jest setup

--- components/HomepageBanner/HomepageBanner.test.tsx
+++ components/HomepageBanner/HomepageBanner.test.tsx
@@ -1,4 +1,3 @@
-import "@testing-library/jest-dom"
 import { render, screen } from "@testing-library/react"
 import { HomepageBanner } from "./HomepageBanner"
 import { EventFrontmatter } from "@/app/events/page"
